09f8f04c505275129ad98f2276b7b4671bb4d545
Okwudili Ezeme <okwudili.ezeme@ab-inbev.com>
2025-02-05 00:24:58 -0500

    solved using set operation

--- find_diff_two_arrays.py
+++ find_diff_two_arrays.py
@@ -30,6 +30,6 @@ Constraints:
 """
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        l1 = [num for num in set(nums1) if num not in set(nums2)]
-        l2 = [num for num in set(nums2) if num not in set(nums1)]
+        l1 = list(set(nums1) - set(nums2))
+        l2 = list(set(nums2) - set(nums1))
         return [l1,l2]
