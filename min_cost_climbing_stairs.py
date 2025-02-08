146dbcd84cc7af96525faa367ace33108c231ba8
Okwudili Ezeme <okwudili.ezeme@ab-inbev.com>
2025-02-08 11:51:54 -0500

    solved min climbing cost

--- min_cost_climbing_stairs.py
+++ min_cost_climbing_stairs.py
@@ -33,13 +33,9 @@ Constraints:
 0 <= cost[i] <= 999"""
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        min_costs = [0] * len(cost)
-        min_costs[-2] = cost[-2]
         for i in range(len(cost)-3, -1,-1):
-            print(f'i = {i}')
-            min_costs[i] = cost[i] + min(min_costs[i+1], min_costs[i+2])
-        print(min_costs)
-        return min(min_costs[0], min_costs[1])
+            cost[i] = cost[i] + min(cost[i+1], cost[i+2])
+        return min(cost[0], cost[1])
 if __name__ == "__main__":
     cost = [1,100,1,1,1,100,1,1,100,1]
     sol = Solution()
