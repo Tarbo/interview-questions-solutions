b6812295472e4fbe150fe3f0d324270aae15bd5b
Okwudili Ezeme <okwudili.ezeme@ab-inbev.com>
2025-02-19 01:31:02 -0500

    solved remove element

--- remove_element.py
+++ remove_element.py
@@ -47,9 +47,16 @@ Constraints:
 0 <= val <= 100"""
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
+        if not nums:
+            return 0
         for index in range(len(nums)):
             if nums[index] == val:
                 nums[index] = 60
-            else:
-                nums[index] = nums[index]
+        nums.sort()
         return len([k for k in nums if k != 60])
+    
+if __name__ == "__main__":
+    nums = [3,2,2,3]
+    val = 3
+    sol = Solution()
+    print(sol.removeElement(nums, val))
